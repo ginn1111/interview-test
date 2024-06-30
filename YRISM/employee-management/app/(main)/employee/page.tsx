2ccf9d9736768444336aac4449e21d8b84586887
ginn1111 <vanthuanjw@gmail.com>
2024-06-30 21:30:53 +0700

    build: fix build

--- YRISM/employee-management/app/(main)/employee/page.tsx
+++ YRISM/employee-management/app/(main)/employee/page.tsx
@@ -1,6 +1,8 @@
 import { fetchPositions } from '@/lib/api/server';
 import EmployeeForm from '../_components/employee-form';
 
+export const dynamic = 'force-dynamic';
+
 const CreateEmployeePage = async () => {
   const positionResponse = await fetchPositions();
   const positions = positionResponse.data.data;
